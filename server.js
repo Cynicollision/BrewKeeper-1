0eae78dc6c9728ff3506fd7164a17588e2c5749b
Sean Normoyle <stn825@gmail.com>
2015-05-26 18:40:03 -0400

    removed stylus dependency

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 ﻿// modules
 var express = require('express'),
-    stylus = require('stylus'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
@@ -8,20 +7,11 @@ var express = require('express'),
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 
-function compile(str, path) {
-    return stylus(str).set('filename', path);
-}
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(stylus.middleware(
-    {
-        src: __dirname + '/public/css',
-        compile: compile
-    }
-));
 
 app.use(express.static(__dirname + '/public'));
 
