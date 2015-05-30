f930e4750e1e2ada79eeefc6f87da9f2eb4b12ac
Sean Normoyle <stn825@gmail.com>
2015-05-30 15:15:54 -0400

    auth/logout

--- public/app/account/mvAuth.js
+++ public/app/account/mvAuth.js
@@ -18,12 +18,34 @@
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
             }).then(function (response) {
                 if (response.data.success) {
-                    
                     mvIdentity.currentUser = response.data.user;
                     dfd.resolve(true);
                 } else {
                     dfd.resolve(false);
                 }
+            });
+
+            return dfd.promise;
+        },
+
+        logoutUser: function () {
+            var dfd = $q.defer();
+            $http({
+                method: 'POST',
+                url: 'logout',
+                data: {
+                    logout: true
+                },
+                transformRequest: function (obj) {
+                    var str = [];
+                    for (var p in obj)
+                        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                    return str.join("&");
+                },
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).then(function () {
+                mvIdentity.currentUser = undefined;
+                dfd.resolve();
             });
 
             return dfd.promise;
--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,4 +1,4 @@
-﻿angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, mvIdentity, mvNotifier, mvAuth) {
+﻿angular.module('app').controller('mvNavBarLoginCtrl', function ($scope, $http, $location, mvIdentity, mvNotifier, mvAuth) {
     
     $scope.identity = mvIdentity;
 
@@ -9,6 +9,15 @@
             } else {
                 mvNotifier.notify('Username/password incorrect.');
             }
+        });
+    };
+
+    $scope.signout = function () {
+        mvAuth.logoutUser().then(function () {
+            $scope.username = "";
+            $scope.password = "";
+            mvNotifier.notify('You have successfully logged out');
+            $location.path('/');
         });
     };
 });
