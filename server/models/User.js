35187bfab09300d44a1037430252f29382e93889
Sean Normoyle <stn825@gmail.com>
2015-06-13 15:05:50 -0400

    authentication fixes

--- server/models/User.js
+++ server/models/User.js
@@ -3,11 +3,15 @@
 
 
 var userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    username: String,
-    salt: String,
-    hashed_pwd: String,
+    firstName: { type: String, required: '{PATH} is required!' },
+    lastName: { type: String, required: '{PATH} is required!' },
+    username: {
+        type: String,
+        required: '{PATH} is required!',
+        index: { unique: true }
+    },
+    salt: { type: String, required: '{PATH} is required!' },
+    hashed_pwd: { type: String, required: '{PATH} is required!' },
     roles: [String]
 });
 
