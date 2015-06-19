27d343c08b0f85fee1cb30bd912ca82af51cb4de
Sean Normoyle <stn825@gmail.com>
2015-06-19 14:13:28 -0400

    use cache for brew detail requests

--- public/app/brews/mvBrewDetailCtrl.js
+++ public/app/brews/mvBrewDetailCtrl.js
@@ -1,3 +1,9 @@
-﻿angular.module('app').controller('mvBrewDetailCtrl', function ($scope, mvBrew, $routeParams) {
-    $scope.brew = mvBrew.get({ _id: $routeParams.id });
+﻿angular.module('app').controller('mvBrewDetailCtrl', function ($scope, mvCachedBrews, $routeParams) {
+    mvCachedBrews.query().$promise.then(function (collection) {
+        collection.forEach(function (brew) {
+            if (brew._id === $routeParams.id) {
+                $scope.brew = brew;
+            }
+        });
+    });
 });
