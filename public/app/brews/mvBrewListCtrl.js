8a997b152f92c2e974677f5dc19a6f3730ee0144
Sean Normoyle <stn825@gmail.com>
2015-06-19 13:32:58 -0400

    sorting/filtering on brew list

--- public/app/brews/mvBrewListCtrl.js
+++ public/app/brews/mvBrewListCtrl.js
@@ -1,3 +1,9 @@
 ﻿angular.module('app').controller('mvBrewListCtrl', function ($scope, mvBrew) {
     $scope.brews = mvBrew.query();
+
+    $scope.sortOptions = [
+        { value: 'name', text: 'Sort by Name' },
+        { value: 'brewedOn', text: 'Sort by Brew Date' }
+    ];
+    $scope.sortOrder = $scope.sortOptions[0].value;
 });
