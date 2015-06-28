6ed1745f07563c4423787b429b5e51acf7396ff3
Sean Normoyle <stn825@gmail.com>
2015-06-28 14:42:57 -0400

    setting up jasmine/karma

---
+++ test/app/brews/AddBrewCtrlSpec.js
@@ -0,0 +1,15 @@
+﻿//var angular = require('angular');
+//    angular.module('app', [
+//    require('angular-animate'),
+//    require('angular-mocks/ngMock'),
+//    require('angular-mocks/ngAnimateMock')
+
+//]);
+
+describe('mvAddBrewCtrl', function () {
+    beforeEach(module('app'));
+    
+    it('Exists?', function () {
+        expect(1).toEqual(1);
+    });
+});
