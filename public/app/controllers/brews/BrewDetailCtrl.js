637c35af6f3366a92d74b1c170fea6c6ae8ead6d
Sean Normoyle <stn825@gmail.com>
2015-07-08 18:58:26 -0400

    working on angular tests

--- public/app/controllers/brews/BrewDetailCtrl.js
+++ public/app/controllers/brews/BrewDetailCtrl.js
@@ -1,12 +1,15 @@
 ﻿angular.module('app').controller('BrewDetailCtrl', function ($scope, $routeParams, BrewService, mvIdentity) {
-    $scope.getCurrentUserBrews = function () {
-        BrewService.queryForUser(mvIdentity.getCurrentUserId()).then(function (response) {
-            response.data.forEach(function (brew) {
-                if (brew._id === $routeParams.id) {
-                    $scope.setCurrentBrew(brew);
-                }
-            });
+    var onGetCurrentUserBrews = function (response) {
+        response.data.forEach(function (brew) {
+            if (brew._id === $routeParams.id) {
+                $scope.setCurrentBrew(brew);
+            }
         });
+    };
+    
+    // TODO: implement BrewService.queryById(brewId), don't retrieve all again
+    $scope.getCurrentUserBrews = function () {
+        BrewService.queryForUser(mvIdentity.getCurrentUserId()).then(onGetCurrentUserBrews);
     };
     
     $scope.setCurrentBrew = function (brew) {
--- public/app/controllers/brews/BrewDetailCtrl_test.js
+++ public/app/controllers/brews/BrewDetailCtrl_test.js
@@ -1,37 +1,41 @@
-﻿describe('BrewDetailCtrl', function () {
-    var $scope, ctrl, BrewServiceMock, mvIdentityMock;
+﻿var dummyResponse = {
+    data: [
+        {
+            _id: 123
+        },
+        {
+            _id: 456
+        }
+    ]
+};
+
+describe('BrewDetailCtrl', function () {
+    var brewDetailScope, succeedPromise, BrewServiceMock, mvIdentityMock;
     
     beforeEach(function () {
         module('app');
         BrewServiceMock = jasmine.createSpyObj('BrewService', ['queryForUser']);
         mvIdentityMock = jasmine.createSpyObj('mvIdentityMock', ['getCurrentUserId']);
         
-        inject(function ($rootScope, $controller, $q, _$timeout_) {
-            $scope = $rootScope.$new();
+        inject(function ($rootScope, $controller, $q) {
+            brewDetailScope = $rootScope.$new();
             
             BrewServiceMock.queryForUser.and.callFake(function () {
-                var deferred = $q.defer(),
-                    dummyResponse = {
-                        data: [
-                            {
-                                id: 123
-                            },
-                            {
-                                id: 456
-                            }
-                        ]
-                    };
-                
-                deferred.resolve(dummyResponse);
-                return deferred.promise;
+                if (succeedPromise) {
+                    return $q.when({});
+                } else {
+                    return $q.reject('Something went wrong');
+                }
             });
+
+
             
             mvIdentityMock.getCurrentUserId.and.callFake(function () {
                 return 82589;
             });
             
-            ctrl = $controller('BrewDetailCtrl', {
-                $scope: $scope,
+            $controller('BrewDetailCtrl', {
+                $scope: brewDetailScope,
                 $routeParams : {
                     id: 123
                 },
@@ -41,13 +45,16 @@
         });
     });
     
-    it('Retrieves a single brew for the current user using the given brew id.', function () {
-        $scope.getCurrentUserBrews();
+    // TODO: move to BrewListCtrl_test after actually implementing retrieve-1-by-id functionality
+    it('Queries all brews for the given user (even though it shouldn\'t)', function () {
+        succeedPromise = true;
+        brewDetailScope.$digest();
         expect(BrewServiceMock.queryForUser).toHaveBeenCalledWith(82589);
+        //expect(brewDetailScope.brew).toBeDefined();
+    });
 
-
-
-
-        //expect($scope.brew).toBeDefined();
+    it('Can set the current brew', function () {
+        brewDetailScope.setCurrentBrew({});
+        expect(brewDetailScope.brew).toBeDefined();
     });
 });
