3d26565f675791e7b78f6593bddf7c3eef458773
Sean Normoyle <stn825@gmail.com>
2015-07-28 21:00:37 -0400

    added to DeleteBrewCtrl_test.js

--- public/app/controllers/brews/DeleteBrewCtrl_test.js
+++ public/app/controllers/brews/DeleteBrewCtrl_test.js
@@ -1,3 +1,28 @@
 ﻿describe('DeleteBrewCtrl', function () {
-    it('NEEDS TESTS', function () { });
+    var $scope, Brew, 
+        mockBrewId = 825;
+        
+    beforeEach(function () {
+        module('BrewKeeper');
+            
+        inject(function ($rootScope, $controller, _Brew_) {
+            $scope = $rootScope.$new();
+            Brew = _Brew_;
+                
+            $controller('DeleteBrewCtrl', {
+                $scope: $scope,
+                $routeParams : {
+                    id: mockBrewId
+                },
+                Brew: Brew
+            });
+        });
+    });
+
+    it('Calls the Brew service to delete the brew.', function () {
+        spyOn(Brew, 'delete').and.callThrough();
+        $scope.onConfirmDelete();
+        expect(Brew.delete).toHaveBeenCalled();
+    });
+
 });
