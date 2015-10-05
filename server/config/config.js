09ad6f6cc1cd14be836c98e04df5245fa24b4aeb
Sean Normoyle <stn825@gmail.com>
2015-10-05 18:56:43 -0400

    updated mongo connection string

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@
             port: 3030
         },
         production: {
-            db: 'mongodb://brewkeeper:keeperbrew@ds061268.mongolab.com:61268/heroku_app37114486',
+            db: ' mongodb://brewkeeper:keeperbrew@ds051833.mongolab.com:51833/heroku_lm132nw3',
             rootPath: rootPath,
             port: process.env.PORT || 80
         }
