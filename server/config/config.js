16b5d2b3c4926b571f3f5b76f4e3623fca43c4fd
Sean Normoyle <stn825@gmail.com>
2015-10-08 18:42:14 -0400

    fix to mongodb url

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@
             port: 3030
         },
         production: {
-            db: ' mongodb://brewkeeper:keeperbrew@ds051833.mongolab.com:51833/heroku_lm132nw3',
+            db: 'mongodb://brewkeeper:keeperbrew@ds051833.mongolab.com:51833/heroku_lm132nw3',
             rootPath: rootPath,
             port: process.env.PORT || 80
         }
