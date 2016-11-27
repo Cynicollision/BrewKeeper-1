77c9924a196db8802a0e9598d1f9f2ed1e817f7e
Cynicollision <stn825@gmail.com>
2016-11-27 15:22:55 -0500

    build application.js

--- public/application-debug.js
+++ public/application-debug.js
@@ -44632,27 +44632,63 @@ if (typeof jQuery === 'undefined') {
                     currentUserId = Identity.getCurrentUserId();
 
                 $q.all([
-                       
+
                     Brew.getCountByUserId(currentUserId).then(function (response) {
-                        basicStats.push({ name: 'Brew count', value: response.data.count });
+                        basicStats.push({
+                            name: 'Brew count',
+                            value: response.data.count,
+                            displayOrder: 1
+                        });
+                    }),
+
+                    Brew.getFirstBrewByDate(currentUserId).then(function (response) {
+
+                        var value = 'No brews!',
+                            brewDate;
+
+                        function formatDate(date) {
+                            return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+                        }
+
+                        if (!response.noBrews) {
+                            value = formatDate(new Date(response.brewDate));
+                        }
+
+                        basicStats.push({
+                            name: 'Brewing since',
+                            value: value,
+                            displayOrder: 3
+                        });
                     }),
 
                     Recipe.getCountByUserId(currentUserId).then(function (response) {
-                        basicStats.push({ name: 'Recipe count', value: response.data.count });
+                        basicStats.push({
+                            name: 'Recipe count',
+                            value: response.data.count,
+                            displayOrder: 2
+                        });
                     }),
                      
                     Recipe.getTopBrewedRecipes(currentUserId).then(function (response) {
-                        var mostBrewedRecipe = response.data[0];
-                        basicStats.push({ name: 'Most Brewed Recipe', value: mostBrewedRecipe.name });
+                        var mostBrewedRecipe = response.data[0],
+                            mostBrewedRecipeName = mostBrewedRecipe ? mostBrewedRecipe.name : 'No recipes!';
+
+                        basicStats.push({
+                            name: 'Most Brewed Recipe',
+                            value: mostBrewedRecipeName,
+                            displayOrder: 4
+                        });
 
                         $scope.mostBrewedRecipes = response.data;
                     }),
-
                 ])
                 .then(function () {
+                    basicStats.sort(function (a, b) {
+                        return a.displayOrder - b.displayOrder;
+                    });
+
                     $scope.basicStats = basicStats;
                 });
-
             });
         }
     ]);
@@ -45226,7 +45262,21 @@ if (typeof jQuery === 'undefined') {
                     });
                 
                     return dfd.promise;
-                }
+                },
+
+                getFirstBrewByDate: function (userId) {
+                    var dfd = $q.defer(),
+                        url = '/api/brew/user/first/' + userId;
+
+                    BrewKeeperApi.get(url).then(function (response) {
+                        var brew = response.data === "" ? { noBrews: true } : response.data;
+                        dfd.resolve(brew);
+                    }, function (response) {
+                        dfd.reject(response.data.reason);
+                    });
+
+                    return dfd.promise;
+                },
             };
         }
     ]);
